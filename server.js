8f9a0c92f375e7a679d6ad180344cb6d2ae91183
Vanisha Sharma <sharmavanisha2005@gmail.com>
2025-08-25 17:26:19 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -57,5 +57,10 @@ app.use("/api/messages", messageRouter);
 // Connect to mongodb
 await connectDB();
 
-const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => console.log("Server is running on PORT: " + PORT));
+if (process.env.NODE_ENV !== "production") {
+  const PORT = process.env.PORT || 5000;
+  server.listen(PORT, () => console.log("Server is running on PORT: " + PORT));
+}
+
+// exporting for vercel
+export default server;
